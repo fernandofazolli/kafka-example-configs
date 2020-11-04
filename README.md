ae0077f030a26fe87099e45192b69cab4863149a
Fernando Yuki Fazolli <fernandoyukifazolli@MacBook-Air-de-Fernando.local>
2020-11-03 23:07:57 -0300

    Criando exemplo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # kafka-example-configs
+
+
+https://emmanuelneri.com.br/2019/06/04/kafka-no-spring-boot/
+
+https://dev.to/kafkabr/kafka-consumindo-registros-com-spring-40h8
+
+docker-compose up
---
+++ src/main/java/br/com/fazolli/kafkaexampleconfigs/KafkaExampleConfigsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fazolli.kafkaexampleconfigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleConfigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleConfigsApplication.class, args);
+	}
+
+}
